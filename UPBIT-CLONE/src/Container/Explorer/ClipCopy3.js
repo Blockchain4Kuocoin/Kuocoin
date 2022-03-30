f6ec213c02e9f8f2d68774a49fca6b3d97297392
skyshr <kiyoungsuh@naver.com>
2022-03-30 20:30:34 +0900

    test

---
+++ UPBIT-CLONE/src/Container/Explorer/ClipCopy3.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+// render() {
+//   const url = window.location.href; // url 복사
+
+//   <CopyToClipboard text={url}>
+//     <button>Copy URL to the clipboard</button>;
+//   </CopyToClipboard>;
+// };
+
+// <CopyToClipboard text={url}
+//           onCopy={() => this.setState({copied: true})}>
+// </CopyToClipboard>
